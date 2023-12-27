81c336d9f967659388f58d5863850d13bee30467
Manisha <flowerdark777@gmail.com>
2023-12-27 14:14:40 +0530

    contextjs file modification

--- src/context.js
+++ src/context.js
@@ -1,8 +1,61 @@
-import React, { Children, useContext, useEffect, useState } from "react";
+// import React, { Children, useContext, useEffect, useState } from "react";
 
-const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+// const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+// const AppContext = React.createContext();
+// const AppProvider = ({ children }) => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [movie, setMovie] = useState([]);
+//   const [isError, setIsError] = useState({ show: false, msg: "" });
+//   const [query, setQuery] = useState("titanic");
 
+//   const getMovies = async (url) => {
+//     setIsLoading(true);
+//     try {
+//       const res = await fetch(url);
+//       const data = await res.json();
+//       console.log(data);
+//       if (data.Response === "True") {
+//         setIsLoading(false);
+//         setIsError({
+//           show: false,
+//           msg: "",
+//         });
+//         setMovie(data.Search);
+//       } else {
+//         setIsError({
+//           show: true,
+//           msg: data.Error,
+//         });
+//       }
+//     } catch (error) {
+//       setIsLoading(false);
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     let timeOut = setTimeout(() => {
+//       getMovies(`${API_URL}&s=${query}`);
+//     }, 800);
+//     return () => clearTimeout(timeOut);
+//   }, [query]);
+//   return (
+//     <AppContext.Provider value={{ isLoading, movie, isError, query, setQuery }}>
+//       {children}
+//     </AppContext.Provider>
+//   );
+// };
+
+// const useGlobalContext = () => {
+//   return useContext(AppContext);
+// };
+
+// export { AppContext, AppProvider, useGlobalContext };
+import React, { useContext, useEffect, useState } from "react";
+
+const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 const AppContext = React.createContext();
+
 const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [movie, setMovie] = useState([]);
@@ -40,6 +93,7 @@ const AppProvider = ({ children }) => {
     }, 800);
     return () => clearTimeout(timeOut);
   }, [query]);
+
   return (
     <AppContext.Provider value={{ isLoading, movie, isError, query, setQuery }}>
       {children}
